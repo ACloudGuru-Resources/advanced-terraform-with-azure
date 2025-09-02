274859a144f03a468f41d2a06970354c34b8ec64
Stefan Nychka <stefan-nychka@pluralsight.com>
2025-09-02 08:05:01 -0600

    Update providers.tf
    benign change, testing edit-ability

--- lab_aks_cluster/providers.tf
+++ lab_aks_cluster/providers.tf
@@ -15,7 +15,7 @@
    skip_provider_registration = true
  }
 
- # Import the Azure Resource Group
+ # Import the Azure Resource Group.
 resource "azurerm_resource_group" "k8s" {
   #name     = "<RESOURCE_GROUP_NAME>"
   #location = "<RESOURCE_GROUP_LOCATION>"
